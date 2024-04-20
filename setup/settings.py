d3043654f6963ff143f7e1b2530ce7b549c786c9
JonathanWallace <136648825+JonathanWallace@users.noreply.github.com>
2024-04-20 08:04:44 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c7+q-%_gd1l6^c&@wrgau2j0d9s%s%+#ocbzdil&3+jle_^@!_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
